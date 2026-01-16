9a09419c0bbec08161ced8c0abdd5f49222af392
Saweli Kudasow <skudasov@hotmail.com>
2026-01-16 22:35:51 +0100

    0.6

--- player.gd
+++ player.gd
@@ -1,20 +1,30 @@
 extends CharacterBody2D
 
-@export var speed := 200.0
-@export var jump_force := 400.0
-@export var gravity := 1000.0
+var max_speed = 100   # максимальная скорость
+var acceleration = 80 # насколько быстро разгоняемся
+var friction = 20     # насколько быстро тормозим
 
 func _physics_process(delta):
-	# Гравитация
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	var direction = Vector2.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
 	
-	# Лево / право
-	var direction := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	velocity.x = direction * speed
-	
-	# Прыжок (только если на земле)
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -jump_force
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	
 	move_and_slide()
+	
+	# повернуть игрока к курсору
+	var mouse_pos = get_global_mouse_position()
+	look_at(mouse_pos)
+	rotation += PI     # разворачиваем, левой стороной
