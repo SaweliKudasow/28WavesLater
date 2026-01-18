65f857a04e4d8b598380405a4a2dc0926760372b
Saweli Kudasow <skudasov@hotmail.com>
2026-01-18 19:44:03 +0100

    0.8

--- bullet.gd
+++ bullet.gd
@@ -1,7 +1,13 @@
 extends CharacterBody2D
 
-var speed = 300
+var speed = 100
 var direction = Vector2.RIGHT
+var lifetime = 4.5
+
+func _ready():
+	# через время удаляем пулю
+	await get_tree().create_timer(lifetime).timeout
+	queue_free()
 
 func _physics_process(_delta):
 	velocity = direction * speed
--- player.gd
+++ player.gd
@@ -4,9 +4,18 @@ var max_speed = 100   # максимальная скорость
 var acceleration = 80 # насколько быстро разгоняемся
 var friction = 20     # насколько быстро тормозим
 @onready var anim = $OnShootAnim
+@onready var appear_anim = $OnAppearAnim
+var start = false
 var bullet = preload("res://bullet.tscn")
 
+func _ready():
+	appear_anim.play("appear")
+
 func _physics_process(delta):
+	# останавливаем движение если игра не началась
+	if not start:
+		return
+	
 	# движение
 	var direction = Vector2.ZERO
 	
@@ -27,26 +36,25 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-	# повернуть игрока к курсору
-	var mouse_pos = get_global_mouse_position()
-	look_at(mouse_pos)
-	rotation += PI     # разворачиваем, левой стороной
-	
-	# если лкм зажата & анимация не играет
-	if Input.is_action_pressed("shoot") and not anim.is_playing():
-		anim.play("attack") # запуск анимации выстрела
-		shoot()             # выстрел пули
-	
-	# экранный перенос
-	var screen_size = get_viewport_rect().size
-	var center = screen_size / 2
-	
-	if position.x < 0 or position.x > screen_size.x or position.y < 0 or position.y > screen_size.y:
-		position = center + (center - position)
+	rotate_player()
+	on_shoot()
+	walls_loop()
 
 func _on_shoot_animation_finished():
 	anim.stop()
 	anim.frame = 0
+
+func rotate_player():
+	# повернуть игрока к курсору
+	var mouse_pos = get_global_mouse_position()
+	look_at(mouse_pos)
+	rotation += PI     # разворачиваем, левой стороной
+
+func on_shoot():
+	# если лкм зажата & анимация не играет
+	if Input.is_action_pressed("shoot") and not anim.is_playing():
+		anim.play("attack") # запуск анимации выстрела
+		shoot()             # выстрел пули
 
 func shoot():
 	# добавляем пулю на сцену
@@ -63,3 +71,16 @@ func shoot():
 	# поворачиваем пулю левой стороной
 	bullet_instance.direction = dir
 	bullet_instance.rotation = dir.angle() + PI
+
+func walls_loop():
+	# экранный перенос
+	var screen_size = get_viewport_rect().size
+	var center = screen_size / 2
+	
+	if position.x < 0 or position.x > screen_size.x or position.y < 0 or position.y > screen_size.y:
+		position = center + (center - position)
+
+func _on_appear_anim_finished():
+	anim.show()
+	appear_anim.hide()
+	start = true
